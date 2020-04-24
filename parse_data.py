c4ec65ad147e96015ebc47d3a4777e1945bff4af
rinkwitz <philip.rinkwitz@protonmail.com>
2020-04-24 22:57:48 +0200

    updates

---
+++ parse_data.py
@@ -0,0 +1,29 @@
+import pathlib
+import pickle
+import numpy as np
+
+dataset_name = 'casp7'
+data_dir_path = pathlib.Path(dataset_name)
+if not pathlib.Path('data').exists():
+    pathlib.Path('data').mkdir()
+
+for path in data_dir_path.iterdir():
+    with open(path, 'r') as file:
+        print('{} ... '.format(str(path)))
+        d = {}
+        id_next, seq_next = False, False
+        for i, line in enumerate(file.readlines()):
+            if id_next:
+                id = line.strip()
+                id_next = False
+            if seq_next:
+                seq = line.strip()
+                seq_next = False
+                d[id] = seq
+            if '[ID]' in line:
+                id_next = True
+            if '[PRIMARY]' in line:
+                seq_next = True
+        dict_path = pathlib.Path('data/dict_{}_{}.pkl'.format(dataset_name, path.name))
+        with open(dict_path, 'wb') as dict_file:
+            pickle.dump(d, dict_file, pickle.HIGHEST_PROTOCOL)
